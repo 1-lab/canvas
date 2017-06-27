e867ca1b0e4e4505fa630fa49126956d4a37cd38
Sangkyu <qsilver7@gmail.com>
2017-06-28 00:44:27 +0900

    fix syntax error

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.get('/', function (request, response) {
 
 app.get('/canvas/:room', function (request, response) {
   fs.readFile('canvas.html', 'utf8', function (error, data) {
-    response.send(ejs.render(data, {
-      room: request.param('room');
+    response.send(ejs.render(data), {
+      room: request.param('room')
     });
   });
 });
