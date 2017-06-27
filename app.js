6155ca5967714f1e650a0c873dfe2c57c6bf1244
Sangkyu <qsilverr@naver.com>
2017-06-28 01:15:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/canvas/:room', function (request, response) {
 });
 
 app.get('/room', function (request, response) {
-  response.send(io.sockets.manager.room);
+  response.send(io.sockets.manager.rooms);
 });
 
 io.sockets.on('connection', function (socket) {
